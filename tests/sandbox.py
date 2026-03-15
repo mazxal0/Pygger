46b2276246c8707893bf5d16404514df2cf88109
Максим <makwest5008@gmail.com>
2026-03-15 23:15:19 +0300

    some fix in .gitignore

--- tests/sandbox.py
+++
@@ -1,7 +0,0 @@
-from pygger import Logger, LogLevel
-
-pygger = Logger(name='pygger', use_color=True)
-
-pygger.log(message="Hello World!", level=LogLevel.WARNING)
-
-pygger.error(message="Error, it's not good")
